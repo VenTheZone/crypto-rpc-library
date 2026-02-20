93860485746369f75693e1320670fa2b42602c88
VenTheZone <kytusdevenn@protonmail.com>
2026-02-20 15:57:02 +0000

    feat: add rpc data types

---
+++ internal/types/rpc.go
@@ -0,0 +1,37 @@
+package types
+
+type RPCStatus string
+
+const (
+	StatusWorking     RPCStatus = "working"
+	StatusNeedsKey    RPCStatus = "needs-key"
+	StatusRateLimited RPCStatus = "rate-limited"
+	StatusDead        RPCStatus = "dead"
+	StatusUntested    RPCStatus = "untested"
+)
+
+type ChainType string
+
+const (
+	ChainEVM     ChainType = "evm"
+	ChainSolana  ChainType = "solana"
+	ChainSui     ChainType = "sui"
+	ChainAptos   ChainType = "aptos"
+	ChainUnknown ChainType = "unknown"
+)
+
+type RPC struct {
+	Name       string    `json:"name"`
+	URL        string    `json:"url"`
+	AuthHeader string    `json:"auth_header"`
+	RPS        float64   `json:"rps"`
+	TPS        float64   `json:"tps"`
+	Mempool    bool      `json:"mempool"`
+	Status     RPCStatus `json:"status"`
+	Chain      ChainType `json:"chain"`
+}
+
+type RPCList struct {
+	Chain ChainType `json:"chain"`
+	RPCs  []RPC     `json:"rpcs"`
+}
